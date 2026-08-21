37d34eef62ecf8207257bb985c2197dc68536519
EduardoAquinta <eddie.ashton@madetech.com>
2026-08-21 15:59:51 +0100

    update: first test created and failed

--- returnsTrue.test.js
+++ returnsTrue.test.js
@@ -1,13 +1,7 @@
 const returnsTrue = require('./returnsTrue');
 
-describe('returnsTrue function', () => {
-  // Test 1: Passing Test
-  test('returns boolean true when called', () => {
-    expect(returnsTrue()).toBe(true);
-  });
-
-  // Test 2: Failing Test
-  test('returns false (intentional failure for practice)', () => {
-    expect(returnsTrue()).toBe(false);
+describe('add', () => {
+  test('returns 0 when given an empty string', () => {
+    expect(add("")).toBe("0");
   });
 });
