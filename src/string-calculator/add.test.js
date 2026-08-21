c8303a0aa297c3a736e19c0f873dc2331f3ae41a
Will Gibson (Made Tech) <261894875+willgibson-madetech@users.noreply.github.com>
2026-08-21 16:06:50 +0100

    Failing: returns 1 when given 1

--- src/string-calculator/add.test.js
+++ src/string-calculator/add.test.js
@@ -4,4 +4,8 @@ describe('add', () => {
   test('returns 0 when given an empty string', () => {
     expect(add("")).toBe("0");
   });
+
+  test('returns 1 when given 1', () => {
+    expect(add("1")).toBe("1");
+  });
 });
