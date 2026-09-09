4874d07b416952c0ffedddaf5d89df0c3c661994
EduardoAquinta <eddie.ashton@madetech.com>
2026-09-09 17:42:12 +0100

    refactor: add method

--- src/string-calculator/add.js
+++ src/string-calculator/add.js
@@ -2,17 +2,21 @@ function add(input) {
   if (input === "") {
     return "0"
   }
-
-  function numberWithoutFloatingPointErrors() {
-    return Math.round(total * 1e10) / 1e10;
-  }
-
+  
   let total = 0;
-  for (const number of input.split("\n").join(",").split(",")) {
+  for (const number of splitByNewLineOrComma()) {
     total+= Number(number);
   }
 
   return numberWithoutFloatingPointErrors().toString()
+
+  function splitByNewLineOrComma() {
+    return input.split("\n").join(",").split(",");
+  }
+
+  function numberWithoutFloatingPointErrors() {
+    return Math.round(total * 1e10) / 1e10;
+  }
 }
 
 module.exports = add;
