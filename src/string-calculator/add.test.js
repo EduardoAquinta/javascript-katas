d28f563594c4d6a79aebb4ea172cc642c6fbd633
Will Gibson (Made Tech) <261894875+willgibson-madetech@users.noreply.github.com>
2026-09-09 17:28:42 +0100

    Refactor: Use parameterised test

--- src/string-calculator/add.test.js
+++ src/string-calculator/add.test.js
@@ -5,19 +5,14 @@ describe('add', () => {
     expect(add("")).toBe("0");
   });
 
-  test('returns 1 when given 1', () => {
-    expect(add("1")).toBe("1");
-  });
-
-  test('returns 5 when given 5', () => {
-    expect(add("5")).toBe("5");
-  });
-
-  test('returns 2 when given 1,1', () => {
-    expect(add("1,1")).toBe("2");
-  });
-
-    test('returns 3.3 when given 1.1, 2.2', () => {
-    expect(add("1.1,2.2")).toBe("3.3");
+  describe('returns sum of comma separated numbers', () => {
+    test.each([
+      ["1", "1"],
+      ["5", "5"],
+      ["1,1", "2"],
+      ["1.1,2.2", "3.3"],
+    ])('returns %s when given %s', (input, expected) => {
+      expect(add(input)).toBe(expected);
+    });
   });
 });
