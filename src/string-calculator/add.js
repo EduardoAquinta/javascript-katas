270d1c70c131b40f924caf781c84fb5423dc25a0
EduardoAquinta <eddie.ashton@madetech.com>
2026-09-11 14:14:42 +0100

    green: test passed for positions of newline and comma

--- src/string-calculator/add.js
+++ src/string-calculator/add.js
@@ -6,6 +6,10 @@ function add(input) {
   if (input.includes(",\n")) {
     return "Number expected but '\n' found at position 6.";
   }
+
+  if (input.includes("\n,")) {
+    return "Number expected but ',' found at position 6.";
+  }
   
   let total = 0;
   for (const number of splitByNewLineOrComma()) {
